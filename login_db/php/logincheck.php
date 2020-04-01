2afe6752554103f773889a45287816fd52bf7aaa
Nafiz1000 <56690613+Nafiz1000@users.noreply.github.com>
2020-04-01 11:21:36 +0600

    Update logincheck.php

--- login_db/php/logincheck.php
+++ login_db/php/logincheck.php
@@ -3,9 +3,9 @@
 	session_start();
 
 	include('../service/functions.php');
-	include_once('db.php');
-	require('db.php');
-	require_once('db.php');
+	//include_once('db.php');
+	//require('db.php');
+	//require_once('db.php');
 
 	if(isset($_POST['submit'])){
 
@@ -16,7 +16,6 @@
 
 		if(count($user) > 0 ){
 			$_SESSION['user'] = $user;
-			//echo "test";
 			header("location: ../views/home.php");
 		}else{
 			echo "invalid username/password";
